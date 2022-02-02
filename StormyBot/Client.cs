55de98b6a307395872a5092061bf8299fead4ab6
drinkingcarrots <poopler9999@gmail.com>
2022-02-01 19:04:54 -0700

    fixed something

---
+++ StormyBot/Client.cs
@@ -0,0 +1,26 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.VoiceNext;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using StormyBot;
+using StormyBot.Commands;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StormyBot
+{
+    public partial class Bot
+    {
+        public async Task RunClientAsync()
+        {
+            
+        }
+    }
+}
